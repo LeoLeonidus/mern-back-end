5f4738a1efbef0a32eed1ace80d15f5c7dfe5975
LeoLeonidus <verzoni.massimo@gmail.com>
2023-06-14 13:47:18 +0200

    mern-beck-end ready to deploy

---
+++ config/allowedOrigins.js
@@ -0,0 +1,12 @@
+/* const allowedOrigins = [
+    'http://192.168.1.106:3000',
+    'http://192.168.1.223',
+    'http://localhost:3000',
+    'https://mern-fron-end.onrender.com/'
+
+] */
+const allowedOrigins = [
+    'https://mern-fron-end.onrender.com/'
+]
+
+module.exports = allowedOrigins
