6bb0cfe3246fb3a1549335e778eeb3b505d4ccde
LeoLeonidus <verzoni.massimo@gmail.com>
2023-06-14 16:42:42 +0200

    backend log

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,9 +2,12 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
+        console.log("origin=",origin);
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            console.log("OK-------");
             callback(null, true)
         } else {
+            console.log("KO-------");
             callback(new Error('Not allowed by CORS'))
         }
     },
